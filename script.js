c3c341ac785af28aa60e0c9e7d43e67a946cdeea
omero2582 <iiomero2582ii@gmail.com>
2022-04-02 00:08:23 -0400

    finish basic UI

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let p1Wins = 0;
 let cpuWins = 0;
+let p1Pick = document.querySelector('#p1Pick');
+let cpuPick = document.querySelector('#cpuPick'); 
+let outputResult = document.querySelector('.resultOutput');
+let outputScoreP1 = document.querySelector('#p1');
+let outputScoreCpu = document.querySelector('#cpu');
+let outputGameEnd = document.querySelector('.gameEndOutput');
 
 function computerPlay (){
     const choice = ['Rock', 'Paper', 'Scissors']; 
@@ -10,48 +16,65 @@ function random(num){
     return (Math.floor(Math.random() * num))
 }
 
-function playRound (p1, cpu1){
+function toFirstCase(string){
+    return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+}
 
+function playRound (p1, cpu1){
+    setBaseTexts();
     p1 = toFirstCase(p1);
     let strOut = '';
 
     console.log(`Player 1: ${p1} \t Computer: ${cpu1}`);
     if (p1 === cpu1){
         //tie
-        strOut = `Its a Tie !!`; 
+        outputResult.textContent = `Its a Tie !!`; 
     }else if (p1 === 'Rock' && cpu1 === 'Scissors' || p1 === 'Paper' && cpu1 === 'Rock' || p1 == 'Scissors' && cpu1 == 'Paper'){
         //p1 win
         p1Wins++;
-        strOut = `p1 Wins !! ${p1} beats ${cpu1}`;
+        outputResult.textContent = `p1 Wins !! ${p1} beats ${cpu1}`;
     }else{
         //cpu1 win
         cpuWins++;
-        strOut = `p1 Loses !! ${cpu1} beats ${p1}`;
+        outputResult.textContent = `p1 Loses !! ${cpu1} beats ${p1}`;
     }
     strOut += `\np1: ${p1Wins} cpu1= ${cpuWins}`;
+    //new
+    
+    p1Pick.textContent+= ` ${p1}`;
+    cpuPick.textContent+= ` ${cpu1}`;
+    outputScoreP1.textContent+= ` ${p1Wins}`;
+    outputScoreCpu.textContent+= ` ${cpuWins}`;
     return strOut
 }
 
-async function game(){
-    let keepLoop = true;
+
+const buttons = document.querySelectorAll('.playerPick');
+buttons.forEach(button => button.addEventListener('click', checkKey));
+
+function checkKey(e){
+  console.log( playRound(this.id, computerPlay()) );
+
+  if (cpuWins == 3 || p1Wins == 3){
+    outputGameEnd.textContent = 'game over?';
     p1Wins = 0;
     cpuWins = 0;
-
-    for (let i = 0; keepLoop; i++) {
-        let p1Select = prompt('type rock, paper, or scissors');
-        console.log( playRound(p1Select, computerPlay()) );
-
-        if (cpuWins == 3 || p1Wins == 3){
-            keepLoop = false;
-        }
-        await new Promise(resolve => setTimeout(resolve, 1000));
     }
 }
 
-
-game();
-
-
-function toFirstCase(string){
-    return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+function game2(){
+    p1Wins = 0;
+    cpuWins = 0;
+    setBaseTexts();
 }
+
+function setBaseTexts(){
+    p1Pick.textContent = 'p1:';
+    cpuPick.textContent = 'cpu:';
+    outputScoreP1.textContent = 'p1:';
+    outputScoreCpu.textContent = 'cpu:';
+    outputResult.textContent = 'Result';
+    outputGameEnd.textContent = 'GameEnd';
+}
+
+game2();
